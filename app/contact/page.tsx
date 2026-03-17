087777d547977939d1546ac2052a01c6c043a520
BENJAMIN IKODIT <bikodit@goldkach.com>
2026-03-17 10:02:25 +0300

    update contactpage

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -250,10 +250,18 @@ export default function ContactPage() {
         </div>
         <map />
       </section>
-      <div className="w-fullgrid grid-col-1 justify-center mt-16">
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.745313320775!2d32.64591957310303!3d0.3466123639828599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177db948a3fc10ab%3A0xa61368735eb67913!2sKireka%20Shopping%20Centre!5e0!3m2!1sen!2sug!4v1773226618219!5m2!1sen!2sug" width="1400" height="450" ></iframe>
-      </div>
+     <div className="w-full grid grid-cols-1 justify-center mt-16">
+  <div className="relative w-full h-[300px] md:h-[450px] rounded-lg overflow-hidden">
     
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.745313320775!2d32.64591957310303!3d0.3466123639828599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177db948a3fc10ab%3A0xa61368735eb67913!2sKireka%20Shopping%20Centre!5e0!3m2!1sen!2sug!4v1773226618219!5m2!1sen!2sug"
+      className="absolute inset-0 w-full h-full border-0"
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+
+  </div>
+</div>
     </div>
   )
 }
